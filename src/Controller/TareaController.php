9724c90b46bc4e530bca01a2e5233c50f3499783
marcosbazan2002@gmail.com <marcosbazanf@hotmail.com>
2025-06-24 20:39:00 +0200

    Depurar codigo

--- src/Controller/TareaController.php
+++ src/Controller/TareaController.php
@@ -13,47 +13,44 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class TareaController extends AbstractController
 {
+    private EntityManagerInterface $em;
+    private TareaRepository $tareaRepository;
 
-    /**
-     * @Route("/tarea", name="tarea")
-     */
-    public function index(): Response
+    public function __construct(EntityManagerInterface $em, TareaRepository $tareaRepository)
     {
-        return $this->render('tarea/index.html.twig', [
-            'controller_name' => 'TareaController',
-            ]);
+        $this->em = $em;
+        $this->tareaRepository = $tareaRepository;
     }
-    /**
-     * @Route("/", name="app_listado_tarea")
-     */
-    public function listado(TareaRepository $tareaRepository): Response
+
+    #[Route('/', name: 'app_listado_tarea', methods: ['GET'])]
+    public function listado(): Response
     {
-        $tareas = $tareaRepository->findAll();
+        $tareas = $this->tareaRepository->findAll();
 
         return $this->render('tarea/listado.html.twig', [
             'tareas' => $tareas,
         ]);
     }
 
-    /**
-     * @Route("/crear-tarea", name="app_crear_tarea", methods={"GET", "POST"})
-     */
-    public function crear(Request $request, EntityManagerInterface $em): Response
+    #[Route('/crear-tarea', name: 'app_crear_tarea', methods: ['GET', 'POST'])]
+    public function crear(Request $request, TareaManager $tareaManager): Response
     {
         $tarea = new Tarea();
-        $descripcion = $request->request->get('descripcion');
 
         if ($request->isMethod('POST')) {
-            if (!empty($descripcion)) {
-                $tarea->setDescripcion($descripcion);
+            $descripcion = $request->request->get('descripcion', '');
 
-                $em->persist($tarea);
-                $em->flush();
+            $tarea->setDescripcion($descripcion);
+            $errores = $tareaManager->validar($tarea);
 
+            if (empty($errores)) {
+                $tareaManager->crear($tarea);
                 $this->addFlash('success', 'Tarea creada correctamente!');
                 return $this->redirectToRoute('app_listado_tarea');
-            } else {
-                $this->addFlash('warning', 'El campo "Descripción" es obligatorio.');
+            }
+
+            foreach ($errores as $error) {
+                $this->addFlash('warning', $error->getMessage());
             }
         }
 
@@ -62,89 +59,18 @@ class TareaController extends AbstractController
         ]);
     }
 
-    /**
-     * @Route("/editar-tarea/{id}", name="app_editar_tarea", requirements={"id"="\d+"}, methods={"GET", "POST"})
-     */
-    public function editar(
-        int $id,
-        TareaRepository $tareaRepository,
-        Request $request,
-        EntityManagerInterface $em
-    ): Response {
-        $tarea = $tareaRepository->find($id);
+    #[Route('/editar-tarea/{id}', name: 'app_editar_tarea', requirements: ['id' => '\d+'], methods: ['GET', 'POST'])]
+    public function editar(int $id, Request $request, TareaManager $tareaManager): Response
+    {
+        $tarea = $this->tareaRepository->find($id);
 
         if (!$tarea) {
             throw $this->createNotFoundException('Tarea no encontrada.');
         }
 
-        $descripcion = $request->request->get('descripcion');
-
         if ($request->isMethod('POST')) {
-            if (!empty($descripcion)) {
-                $tarea->setDescripcion($descripcion);
+            $descripcion = $request->request->get('descripcion', '');
 
-                $em->flush();
-
-                $this->addFlash('success', 'Tarea editada correctamente!');
-                return $this->redirectToRoute('app_listado_tarea');
-            } else {
-                $this->addFlash('warning', 'El campo "Descripción" es obligatorio.');
-            }
-        }
-
-        return $this->render('tarea/editar.html.twig', [
-            'tarea' => $tarea,
-        ]);
-    }
-
-    /**
-     * @Route("/eliminar-tarea/{id}", name="app_eliminar_tarea", requirements={"id"="\d+"}, methods={"POST"})
-     */
-    public function eliminar(Tarea $tarea, EntityManagerInterface $em): Response
-    {
-        $em->remove($tarea);
-        $em->flush();
-
-        $this->addFlash('success', 'Tarea eliminada correctamente!');
-        return $this->redirectToRoute('app_listado_tarea');
-    }
-
-    /**
-     * @Route("/crear-tarea-servicio", name="app_crear_tarea_servicio", methods={"GET", "POST"})
-     */
-    public function crearServicio(TareaManager $tareaManager, Request $request): Response
-    {
-        $descripcion = $request->request->get('descripcion');
-        $tarea = new Tarea();
-
-        if ($request->isMethod('POST')) {
-            $tarea->setDescripcion($descripcion);
-            $errores = $tareaManager->validar($tarea);
-
-            if (empty($errores)) {
-                $tareaManager->crear($tarea);
-                $this->addFlash('success', 'Tarea creada correctamente!');
-                return $this->redirectToRoute('app_listado_tarea');
-            }
-
-            foreach ($errores as $error) {
-                $this->addFlash('warning', $error->getMessage());
-            }
-        }
-
-        return $this->render('tarea/crear.html.twig', [
-            'tarea' => $tarea,
-        ]);
-    }
-
-    /**
-     * @Route("/editar-tarea-servicio/{id}", name="app_editar_tarea_servicio", requirements={"id"="\d+"}, methods={"GET", "POST"})
-     */
-    public function editarServicio(TareaManager $tareaManager, Tarea $tarea, Request $request): Response
-    {
-        $descripcion = $request->request->get('descripcion');
-
-        if ($request->isMethod('POST')) {
             $tarea->setDescripcion($descripcion);
             $errores = $tareaManager->validar($tarea);
 
@@ -164,10 +90,8 @@ class TareaController extends AbstractController
         ]);
     }
 
-    /**
-     * @Route("/eliminar-tarea-servicio/{id}", name="app_eliminar_tarea_servicio", requirements={"id"="\d+"}, methods={"POST"})
-     */
-    public function eliminarServicio(Tarea $tarea, TareaManager $tareaManager): Response
+    #[Route('/eliminar-tarea/{id}', name: 'app_eliminar_tarea', requirements: ['id' => '\d+'], methods: ['POST'])]
+    public function eliminar(Tarea $tarea, TareaManager $tareaManager): Response
     {
         $tareaManager->eliminar($tarea);
 
